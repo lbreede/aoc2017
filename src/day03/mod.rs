3fe001875792a3722b2ed833dce1fbaef6c987a1
lbreede <67719520+lbreede@users.noreply.github.com>
2024-10-19 16:52:43 -0700

    solved day 3

--- src/day03/mod.rs
+++ src/day03/mod.rs
@@ -10,9 +10,12 @@ pub fn main() {
     println!("Part 1: {} ({:.2?})", result, elapsed);
 
     let now = Instant::now();
-    let result = part_two(INPUT);
-    let elapsed = now.elapsed();
-    println!("Part 2: {} ({:.2?})\n", result, elapsed);
+    if let Some(result) = part_two(INPUT) {
+        let elapsed = now.elapsed();
+        println!("Part 2: {} ({:.2?})\n", result, elapsed);
+    } else {
+        println!("Part 2: Could not solve");
+    }
 }
 
 fn part_one(input: u32) -> u32 {
@@ -21,7 +24,7 @@ fn part_one(input: u32) -> u32 {
         diameter += 1;
     }
 
-    let dist_to_corner = (0..4)
+    let dist_to_corner = (0..5)
         .map(|i| {
             let corner = diameter * diameter - i * (diameter - 1);
             corner.abs_diff(input)
@@ -31,8 +34,20 @@ fn part_one(input: u32) -> u32 {
     diameter - 1 - dist_to_corner
 }
 
-fn part_two(input: u32) -> u32 {
-    0
+fn part_two(input: u32) -> Option<u32> {
+    let mut sequence = include_str!("b141481.txt").lines();
+    sequence.next();
+    sequence.next();
+    let mut sequence = sequence.map(|line| {
+        let (_, n) = line.split_once(" ").unwrap();
+        n.parse::<u32>().unwrap()
+    });
+    while let Some(n) = sequence.next() {
+        if n > input {
+            return Some(n);
+        }
+    }
+    None
 }
 
 #[cfg(test)]
@@ -52,13 +67,8 @@ mod tests {
         assert_eq!(part_one(INPUT), 371);
     }
 
-    // #[test]
-    // fn test_part_two_example() {
-    // assert_eq!(part_two(example), 9);
-    // }
-    //
-    // #[test]
-    // fn test_part_two() {
-    // assert_eq!(part_two(INPUT), 244);
-    // }
+    #[test]
+    fn test_part_two() {
+        assert_eq!(part_two(INPUT), Some(369601));
+    }
 }

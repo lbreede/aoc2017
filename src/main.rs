3611bf964c5b6a8cc2097d94386d9c0f70006cbc
lbreede <67719520+lbreede@users.noreply.github.com>
2024-10-19 00:12:46 -0700

    update README.md generator

--- src/main.rs
+++ src/main.rs
@@ -14,15 +14,18 @@ fn main() {
     let result01 = day01::main();
     let result02 = day02::main();
 
-    if &args[1] == "readme" {
-        println!("# Advent of Code 2017\n");
-        println!("| Day | Part 1 | Part 2 |");
-        println!("|----:| ------ | ------ |");
-        result01.readme();
-        result02.readme();
-    } else {
-        println!("Advent of Code 2017\n");
-        println!("{}", result01);
-        println!("{}", result02);
+    match args.get(1) {
+        Some(arg) if arg == "readme" => {
+            println!("# Advent of Code 2017 :christmas_tree:\n\n### Durations\n");
+            println!("| Day | Part 1 | Part 2 |");
+            println!("|----:| ------ | ------ |");
+            result01.readme();
+            result02.readme();
+        }
+        _ => {
+            println!("Advent of Code 2017\n");
+            println!("{}", result01);
+            println!("{}", result02);
+        }
     }
 }

fb72fb1f2e6b10b34c13a6a35434ca3d72cf64c7
lbreede <67719520+lbreede@users.noreply.github.com>
2024-10-19 15:45:37 -0700

    solved day 3 part 1

--- src/day03/mod.rs
+++ src/day03/mod.rs
@@ -1,6 +1,6 @@
 use std::time::Instant;
 
-const INPUT: &str = include_str!("input.txt");
+const INPUT: u32 = 368_078;
 
 pub fn main() {
     println!("Day 3: Spiral Memory");
@@ -15,11 +15,23 @@ pub fn main() {
     println!("Part 2: {} ({:.2?})\n", result, elapsed);
 }
 
-fn part_one(input: &str) -> u32 {
-    0
+fn part_one(input: u32) -> u32 {
+    let mut diameter = (input as f32).sqrt().ceil() as u32;
+    if diameter % 2 == 0 {
+        diameter += 1;
+    }
+
+    let dist_to_corner = (0..4)
+        .map(|i| {
+            let corner = diameter * diameter - i * (diameter - 1);
+            corner.abs_diff(input)
+        })
+        .min()
+        .unwrap();
+    diameter - 1 - dist_to_corner
 }
 
-fn part_two(input: &str) -> u32 {
+fn part_two(input: u32) -> u32 {
     0
 }
 
@@ -29,23 +41,24 @@ mod tests {
 
     #[test]
     fn test_part_one_example() {
-        let example = include_str!("example.txt");
-        assert_eq!(part_one(example), 18);
+        assert_eq!(part_one(1), 0);
+        assert_eq!(part_one(12), 3);
+        assert_eq!(part_one(23), 2);
+        assert_eq!(part_one(1024), 31);
     }
 
     #[test]
     fn test_part_one() {
-        assert_eq!(part_one(INPUT), 36_174);
+        assert_eq!(part_one(INPUT), 371);
     }
 
-    #[test]
-    fn test_part_two_example() {
-        let example = include_str!("example2.txt");
-        assert_eq!(part_two(example), 9);
-    }
-
-    #[test]
-    fn test_part_two() {
-        assert_eq!(part_two(INPUT), 244);
-    }
+    // #[test]
+    // fn test_part_two_example() {
+    // assert_eq!(part_two(example), 9);
+    // }
+    //
+    // #[test]
+    // fn test_part_two() {
+    // assert_eq!(part_two(INPUT), 244);
+    // }
 }

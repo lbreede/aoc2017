a95eb6d1f412aa4774ae855b90f451794c49c93f
lbreede <67719520+lbreede@users.noreply.github.com>
2024-10-19 17:12:36 -0700

    solved day 4

--- src/day04/mod.rs
+++ src/day04/mod.rs
@@ -16,17 +16,33 @@ pub fn main() {
 }
 
 fn is_valid(passphrase: &str) -> bool {
-    let words_vec: Vec<&str> = passphrase.split_whitespace().collect();
-    let words_set: HashSet<&str> = HashSet::from_iter(words_vec.iter().cloned());
-    words_vec.len() == words_set.len()
+    let mut words_set = HashSet::new();
+    for word in passphrase.split_whitespace() {
+        if !words_set.insert(word) {
+            return false; // Duplicate found
+        }
+    }
+    true
 }
 
 fn part_one(input: &str) -> u32 {
-    0
+    input.lines().filter(|&line| is_valid(line)).count() as u32
+}
+
+fn is_valid2(passphrase: &str) -> bool {
+    let mut set = HashSet::new();
+    for word in passphrase.split_whitespace() {
+        let mut chars: Vec<char> = word.chars().collect();
+        chars.sort();
+        if !set.insert(chars.iter().collect::<String>()) {
+            return false;
+        }
+    }
+    true
 }
 
 fn part_two(input: &str) -> u32 {
-    0
+    input.lines().filter(|&line| is_valid2(line)).count() as u32
 }
 
 #[cfg(test)]
@@ -40,19 +56,22 @@ mod tests {
         assert!(is_valid("aa bb cc dd aaa"));
     }
 
-    // #[test]
-    // fn test_part_one() {
-    // assert_eq!(part_one(INPUT), 36_174);
-    // }
-    //
-    // #[test]
-    // fn test_part_two_example() {
-    // let example = include_str!("example2.txt");
-    // assert_eq!(part_two(example), 9);
-    // }
-    //
-    // #[test]
-    // fn test_part_two() {
-    // assert_eq!(part_two(INPUT), 244);
-    // }
+    #[test]
+    fn test_part_one() {
+        assert_eq!(part_one(INPUT), 337);
+    }
+
+    #[test]
+    fn test_part_two_example() {
+        assert!(is_valid2("abcde fghij"));
+        assert!(!is_valid2("abcde xyz ecdab"));
+        assert!(is_valid2("a ab abc abd abf abj"));
+        assert!(is_valid2("iiii oiii ooii oooi oooo"));
+        assert!(!is_valid2("oiii ioii iioi iiio"));
+    }
+
+    #[test]
+    fn test_part_two() {
+        assert_eq!(part_two(INPUT), 231);
+    }
 }
